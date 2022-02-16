28c47fe065460faa2c89a1162d06ab3925ccdeb5
tptrw61 <tptrw.61@gmail.com>
2022-02-16 13:27:59 -0700

    made register option more versitile

--- menu.c
+++ menu.c
@@ -1,23 +1,26 @@
 #include "menu.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 #define MENU_FIXED 1
 #define MENU_VARIABLE 2
 
 struct menu_s {
     char type;
+    int size;
 };
 
 typedef struct {
     char name[MENU_OPTION_NAME_SIZE];
     void (*func)(int, void *);
+    int willCont;
 } Item;
 
 typedef struct {
     char type;
-
     int size;
-    int next;
+
+    int max;
     Item *array;
 } MenuFixed;
 
@@ -28,17 +31,21 @@ typedef struct Node {
 
 typedef struct {
     char type;
-
     int size;
+
     Node *list;
     Node *last;
 } MenuVariable;
 
-static int _registerOptionFixed(MenuFixed *menu, const char *name, void (*func)(int, void *));
-static int _registerOptionVariable(MenuVariable *menu, const char *name, void (*func)(int, void *));
+static int _registerOptionFixed(MenuFixed *menu, const char *name, void (*func)(int, void *), int willCont);
+static int _registerOptionVariable(MenuVariable *menu, const char *name, void (*func)(int, void *), int willCont);
 
-static int _displayMenuFixed(MenuFixed *menu, void *param);
-static int _displayMenuVariable(MenuVariable *menu, void *param);
+static void _printMenuFixed(MenuFixed *menu);
+static void _printMenuVariable(MenuVariable *menu);
+
+//return 0 if its an exit function
+static int _chooseMenuFixed(MenuFixed *menu, int choice, void *param);
+static int _chooseMenuVariable(MenuVariable *menu, int choice, void *param);
 
 static void _clearMenuFixed(MenuFixed *menu);
 static void _clearMenuVariable(MenuVariable *menu);
@@ -54,28 +61,59 @@ int menu_registerOption(Menu *menu, const char *optionName, void (*optionFunc)(i
     strncpy(name, optionName, MENU_OPTION_NAME_SIZE-1);
     name[MENU_OPTION_NAME_SIZE-1] = '\0';
     if (menu->type == MENU_FIXED) {
-        return _registerOptionFixed((MenuFixed *)menu, name, optionFunc);
+        return _registerOptionFixed((MenuFixed *)menu, name, optionFunc, 1);
     }
     if (menu->type == MENU_VARIABLE) {
-        return _registerOptionVariable((MenuVariable *) menu, name, optionFunc);
+        return _registerOptionVariable((MenuVariable *) menu, name, optionFunc, 1);
+    }
+    return 0;
+}
+
+int menu_registerExitOption(Menu *menu, const char *optionName, void (*optionFunc)(int, void *)) {
+    if (menu == NULL || optionName == NULL || optionFunc == NULL) {
+        return 0;
+    }
+    char name[MENU_OPTION_NAME_SIZE];
+    strncpy(name, optionName, MENU_OPTION_NAME_SIZE-1);
+    name[MENU_OPTION_NAME_SIZE-1] = '\0';
+    if (menu->type == MENU_FIXED) {
+        return _registerOptionFixed((MenuFixed *)menu, name, optionFunc, 0);
+    }
+    if (menu->type == MENU_VARIABLE) {
+        return _registerOptionVariable((MenuVariable *) menu, name, optionFunc, 0);
     }
     return 0;
 }
 
 void menu_displayMenu(Menu *menu, void *param) {
     if (menu == NULL) {
-        return 0;
+        return;
     }
-    if (menu->type == MENU_FIXED) {
-        _displayMenuFixed((MenuFixed *)menu, param);
-    } else if (menu->type == MENU_VARIABLE) {
-        _displayMenuVariable((MenuVariable *) menu, param);
-    }
+    int choice;
+    int cont;
+    do {
+        if (menu->type == MENU_FIXED) {
+            _printMenuFixed((MenuFixed *)menu);
+        } else if (menu->type == MENU_VARIABLE) {
+            _printMenuVariable((MenuVariable *) menu);
+        }
+        printf("Enter your selection: ");
+        scanf("%d", &choice);
+        if (choice < 0 || choice > menu->size) {
+            printf("Invalid Choice\n");
+            continue;
+        }
+        if (menu->type == MENU_FIXED) {
+            cont = _chooseMenuFixed((MenuFixed *)menu, choice, param);
+        } else if (menu->type == MENU_VARIABLE) {
+            cont = _chooseMenuVariable((MenuVariable *) menu, choice, param);
+        }
+    } while (cont);
 }
 
 void menu_clearMenu(Menu *menu) {
     if (menu == NULL) {
-        return 0;
+        return;
     }
     if (menu->type == MENU_FIXED) {
         _clearMenuFixed((MenuFixed *)menu);
@@ -86,7 +124,7 @@ void menu_clearMenu(Menu *menu) {
 
 void menu_destroy(Menu *menu) {
     if (menu == NULL) {
-        return 0;
+        return;
     }
     if (menu->type == MENU_FIXED) {
         _destroyFixed((MenuFixed *)menu);
--- menu.h
+++ menu.h
@@ -5,13 +5,14 @@
 
 typedef struct menu_s Menu;
 
-//the 'exit' option is never required to be manually added
 Menu *menu_createVariable(void);
 Menu *menu_createFixed(int options);
 
 //optionName is limited to MENU_OPTION_NAME_SIZE-1 chars
 //optionsFunc gets passed int the chosen option along with a void* for any other parameters you want
 int menu_registerOption(Menu *menu, const char *optionName, void (*optionFunc)(int, void *));
+//when this option is chosen, the menu will exit out of the menu loop AFTER optionFunc is called
+int menu_registerExitOption(Menu *menu, const char *optionName, void (*optionFunc)(int, void *));
 
 void menu_displayMenu(Menu *menu, void *param);
 
